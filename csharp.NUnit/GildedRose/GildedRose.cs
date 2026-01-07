1a09bb95d7b389e2b712ed4006b0d1bc30536d19
Sara Fadel <sarafadel@clubbertv.com>
2026-01-08 00:07:08 +0200

    remove unnecessary operation to reveal intention

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -93,7 +93,7 @@ public class GildedRose
             {
                 if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    Items[i].Quality = 0;
                 }
                 else
                 {
