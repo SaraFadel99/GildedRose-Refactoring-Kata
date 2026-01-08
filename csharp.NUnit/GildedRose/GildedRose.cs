57724f5f7bb8f5edf13c6f3556e68790b04b557b
Sara Fadel <sarafadel@clubbertv.com>
2026-01-08 15:05:49 +0200

    rename method

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -38,7 +38,7 @@ public class GildedRose
         {
             if (Items[i].Name == "Aged Brie") 
             {
-                expiredAgedBriedQualityRules(i);
+                agedBrieQualityRules(i);
             }
             else 
             {
@@ -86,7 +86,7 @@ public class GildedRose
         {
             if (Items[i].Name == "Aged Brie")
             {
-                expiredAgedBriedQualityRules(i);
+                agedBrieQualityRules(i);
             }
 
             else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
@@ -100,7 +100,7 @@ public class GildedRose
         }   
     }
 
-    private void expiredAgedBriedQualityRules(int i)
+    private void agedBrieQualityRules(int i)
     {
         if (Items[i].Quality < 50)
         {
